5be893cfed9b95932fb55bca3f7426726369ed4b
zhengqingya <960869719@qq.com>
2019-07-01 18:09:01 +0800

    【上传jar包init】

--- src/main/java/com/github/zhengqing/Application.java
+++ src/main/java/com/github/zhengqing/Application.java
@@ -3,7 +3,7 @@ package com.github.zhengqing;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("---------------进来了...---------------");
+        System.out.println("---------------come...---------------");
     }
 
 }
