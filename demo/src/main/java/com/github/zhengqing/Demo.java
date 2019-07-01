afb2dcceb282532228d1bda83bc01bfd73893ab2
zhengqingya <960869719@qq.com>
2019-07-01 16:40:48 +0800

    【上传jar包init】

--- demo/src/main/java/com/github/zhengqing/Demo.java
+++
@@ -1,9 +0,0 @@
-package com.github.zhengqing;
-
-public class Demo {
-
-    public static void main(String[] args) {
-        System.out.println("-----------上传jar包进来了...-----------");
-    }
-
-}
---
+++ src/main/java/com/github/zhengqing/Application.java
@@ -0,0 +1,9 @@
+package com.github.zhengqing;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("---------------进来了...---------------");
+    }
+
+}
